13367382f235a3e00eb013e0e37cdc39be034781
Gustavo6900 <gustavopassianotocod@gmail.com>
2024-03-01 15:58:12 -0400

    criaçao do header e footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 export default function App() {
 
-  const nome = "João";
-  const sobreNome = "Santos";
-
+  
+  
   return(
-    <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-    </div>  
-  );
 
+    <div>
+   
+      <Header/>
+     
+      <Footer/>
+      </div>  
+  
+  );
 
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+    return(
+        <fotter>
+            <p1>Gustavo Alexandre Passianoto</p1>
+        </fotter>
+    );
+}
